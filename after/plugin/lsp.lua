0d622bf73c0c51257548d474251502d00b2cc0ef
bret-walda <awabkhansky@gmail.com>
2023-08-25 14:32:10 +0500

    Update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,107 +1,16 @@
--- Lsp-zero
-local lsp_zero = require("lsp-zero").preset("recommended")
-local cmp = require("cmp")
-local lspconfig = require("lspconfig")
-local lsnip = require("luasnip")
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Luasnip
-require("luasnip.loaders.from_lua").load()
-lsnip.setup({
-    history = true,
-    update_events = { "TextChanged", "TextChangedI" },
-    enable_autosnippets = true,
-})
+local lsp = require('lsp-zero').preset({})
 
-lsp_zero.on_attach(function(_, bufnr)
-    lsp_zero.default_keymaps({ buffer = bufnr })
-    vim.keymap.set(
-        "n",
-        "gr",
-        "<Cmd>Telescope lsp_references<CR>",
-        { buffer = true, desc = "Show references in a Telescope window." }
-    )
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
 end)
 
--- Language servers
-lsp_zero.ensure_installed({
-    "lua_ls",
-})
-lspconfig.lua_ls.setup({
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim", "s", "t", "i", "d", "c", "sn", "f" },
-            },
-            format = {
-                enable = false,
-            },
-        },
-    },
-})
-lspconfig.clangd.setup({
-    capabilities = capabilities,
-    arguments = {'-Wall'},
+-- When you don't have mason.nvim installed
+-- You'll need to list the servers installed in your system
+lsp.setup_servers({'clangd',})
 
-filetypes = { "c", "cpp", "objc", "objcpp" },
-})
--- Completion setup
-cmp.setup({
-    sources = {
-        { name = "luasnip", option = { show_autosnippets = true } },
-        { name = "nvim_lua" },
-        { name = "nvim_lsp" },
-        { name = "path" },
-    },
-    mapping = {
-        --- @param fallback function
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif lsnip.expand_or_jumpable() then
-                lsnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { "i", "s" }), -- Tab autocomplete
-        --- @param fallback function
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif lsnip.jumpable(-1) then
-                lsnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Enter to complete
-        ["<Up>"] = cmp.mapping.abort(), -- No up and down selection
-        ["<Down>"] = cmp.mapping.abort(),
-        --- @param fallback function
-        ["<C-l>"] = cmp.mapping(function(fallback) -- Move choice forward
-            if lsnip.choice_active() then
-                lsnip.change_choice()
-            else
-                fallback()
-            end
-        end),
-        --- @param fallback function
-        ["<C-h>"] = cmp.mapping(function(fallback) -- Move choice backward
-            if lsnip.choice_active() then
-                lsnip.change_choice(-1)
-            else
-                fallback()
-            end
-        end),
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    -- Expands snippets
-    snippet = {
-        expand = function(args) lsnip.lsp_expand(args.body) end,
-    },
-})
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
-
-lsp_zero.setup()
+lsp.setup()
