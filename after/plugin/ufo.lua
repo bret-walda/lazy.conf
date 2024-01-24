f9b6323c5f7321de07d99d15dc7c19d1e5d5abc6
bret-walda <awabkhansky@gmail.com>
2024-01-24 17:25:08 +0500

    using treesitter now instead of lsp

--- after/plugin/ufo.lua
+++ after/plugin/ufo.lua
@@ -1,5 +1,3 @@
-require('ufo').setup()
-
 vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
@@ -8,6 +6,13 @@ vim.o.foldenable = true
 -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
 vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
 vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+require('ufo').setup({
+    provider_selector = function (bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end
+})
+
 
 --zf to create a fold
 --zo to open the fold
