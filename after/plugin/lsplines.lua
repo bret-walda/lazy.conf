cab561f6ee38172cab043ad53c5b67045f02e6c6
bret-walda <awabkhansky@gmail.com>
2024-02-13 18:50:48 +0500

    added a toggle to lsp_lines plugin

--- after/plugin/lsplines.lua
+++ after/plugin/lsplines.lua
@@ -1,8 +1,27 @@
 require("lsp_lines").setup()
--- Disable virtual_text since it's redundant due to lsp_lines.
-vim.diagnostic.config({
-  virtual_text = false,
-  virtual_lines = true,
-})
 
-vim.keymap.set("","<leader>lp",require("lsp_lines").toggle)
+-- Configure diagnostic display settings
+vim.diagnostic.config({
+    virtual_text = false,
+    virtual_lines = true,
+})
+local is_on = false
+
+local function configure_virtual_lines()
+    if is_on then
+        vim.diagnostic.config({
+            virtual_lines = { only_current_line = true }
+        })
+        is_on = false
+    else
+        vim.diagnostic.config({
+            virtual_lines = { only_current_line = false }
+        })
+        is_on = true
+    end
+end
+
+-- Key mappings
+vim.keymap.set("", "<leader>lp", require("lsp_lines").toggle)
+vim.keymap.set("", "<leader>lc", configure_virtual_lines, {noremap = true, silent = true})
+
